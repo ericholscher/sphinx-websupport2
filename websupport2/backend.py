2bc9d5bdccb0774675682cf9b28604629215c6af
Eric Holscher <eric@ericholscher.com>
2013-07-24 17:50:31 -0700

    Add WebStorage to client package.

---
+++ websupport2/backend.py
@@ -0,0 +1,26 @@
+import json
+import requests
+
+from sphinx.websupport.storage import StorageBackend
+
+class WebStorage(StorageBackend):
+    """
+    A storage class meant to be used by the Sphinx Builder to store nodes.
+
+    This is super inefficient and a hack right now.
+    When in prod we'll store all nodes to be added and send them all up at once like with sync_versions.
+
+    """
+
+    def has_node(self, id):
+        url = "http://localhost:8000/websupport/_has_node"
+        data = {'node_id': id,}
+        r = requests.get(url, params=data)
+        return r.json['exists']
+
+    def add_node(self, id, document, source):
+        url = "http://localhost:8000/websupport/_add_node"
+        data = {'id': id, 'document': document, 'source': source}
+        headers = {'Content-type': 'application/json'}
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        return True
--- websupport2/websupport2.py
+++ websupport2/websupport2.py
@@ -6,7 +6,7 @@ from sphinx.util import copy_static_entry
 from sphinx.util.console import bold
 from sphinx.util.osutil import copyfile
 from translator import UUIDTranslator
-from websupport.backend import DjangoStorage, WebStorage
+from websupport2.backend import WebStorage
 
 import os
 
