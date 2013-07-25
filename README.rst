de44b7c8ba88caae752725dadacc3f0790975bee
Eric Holscher <eric@ericholscher.com>
2013-07-24 17:30:14 -0700

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,3 @@
+A package that provides a client for Sphinx `Websupport`_
+
+.. _Websupport: http://sphinx-doc.org/websupport.html
---
+++ setup.py
@@ -0,0 +1,23 @@
+import codecs
+try:
+    from setuptools import setup, find_packages
+    extra_setup = dict(
+        install_requires = ['django'],
+        )
+except ImportError:
+    from distutils.core import setup
+    extra_setup = {}
+
+setup(
+    name='sphinx-websupport2',
+    version='1.2.3',
+    author='Eric Holscher',
+    author_email='eric@ericholscher.com',
+    url='http://github.com/ericholscher/sphinx-websupport2',
+    license='BSD',
+    description='Improved Client for Sphinx Websupport package.',
+    package_dir={'': '.'},
+    packages=find_packages('.'),
+    long_description=codecs.open("README.rst", "r", "utf-8").read(),
+    **extra_setup
+)
